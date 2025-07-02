4178c4cc984b8ca97720635e992faeafc9356906
alohamat <matalohazk@gmail.com>
2025-07-01 22:07:41 -0300

    Primeiro commit, estrutura do projeto

---
+++ api/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+)
+
+type Item struct {
+	Nome string `json:"nome"`
+	Tempo DataDetalhes `json:"tempo"`
+	Preco int `json:"preco"`
+}
+
+type DataDetalhes struct {
+	Horas int `json:"horas"`
+	Minutos int `json:"minutos"`
+	Segundos int `json:"segundos"`
+	Anos int `json:"anos"`
+}
+
+func main() {
+	agora := time.Now()
+
+	itemexeplo := Item {
+		Nome: "Leite",
+		Tempo: DataDetalhes{
+			Horas: agora.Hour(),
+			Minutos: agora.Minute(),
+			Segundos: agora.Second(),
+			Anos: agora.Year(),
+		},
+		Preco: 1000,
+	}
+	fmt.Println(itemexeplo)
+}
