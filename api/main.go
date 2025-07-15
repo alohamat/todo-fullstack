f3095e862ebe25eb22bfe282ebd6c7b91dcf8de3
alohamat <matalohazk@gmail.com>
2025-07-15 18:56:07 -0300

    feat: cors estavel

--- api/main.go
+++ api/main.go
@@ -2,11 +2,10 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"time"
 
-	"go.mongodb.org/mongo-driver/mongo"
-	"go.mongodb.org/mongo-driver/mongo/options"
+	
+
 )
 
 type Item struct {
---
+++ api/middlewares/cors.go
@@ -0,0 +1,15 @@
+package handlers
+
+import (
+	"github.com/rs/cors"
+	"net/http"
+)
+
+func CorsMiddleware(handler http.Handler) http.Handler {
+	return cors.New(cors.Options{
+		AllowedOrigins: []string{"*"}, // permite que todo mundo chama a api
+		AllowedMethods: []string{"POST", "GET", "PUT", "DELETE"}, // permite metodos que a api aceita
+		AllowedHeaders: []string{"Content-Type"}, // permite headers
+		AllowCredentials: true, // permite credenciais como cookies
+	}).Handler(handler)
+}
