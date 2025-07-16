ee9314b51bb945bb850539a77304c6bdcb7c7614
alohamat <matalohazk@gmail.com>
2025-07-16 10:55:18 -0300

    herosection, feature card, start to carrousel

--- frontend/src/components/HeroSection.tsx
+++ frontend/src/components/HeroSection.tsx
@@ -8,12 +8,12 @@ type HeroProps = {
 
 function HeroSection({title, description, buttonLabel, onButtonClick}: HeroProps) {
     return (
-        <section>
-            <h1 className="">{title}</h1>
-            <p className="">{description}</p>
+        <section className="bg-zinc-400">
+            <h1 className="text-7xl font-bold">{title}</h1>
+            <p className="text-3xl font-medium m-4">{description}</p>
             <button 
                 onClick={onButtonClick}
-                className=""
+                className="bg-amber-300 px-2 y-4 h-15 w-75 font-medium text-2xl rounded-2xl hover:cursor-pointer hover:bg-amber-500 transition ease-in-out"
             >{buttonLabel}</button>
         </section>
     );
