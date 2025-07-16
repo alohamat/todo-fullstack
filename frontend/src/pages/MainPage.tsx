d6bcd8984750798b2cd8f5790052ed5132f33c62
alohamat <matalohazk@gmail.com>
2025-07-16 09:54:30 -0300

    feat: initialized navbar

--- frontend/src/pages/MainPage.tsx
+++ frontend/src/pages/MainPage.tsx
@@ -1,9 +1,17 @@
 function MainPage() {
   return (
     <div className="bg-zinc-800 min-h-screen min-w-screen">
-      <div className="flex items-center bg-zinc-300 w-screen">
-        <h1 className="p-4">Meu app</h1>
-      </div>
+      <header className="sticky top-0 flex items-center gap-4 bg-zinc-300 w-screen p-4 h-20">
+        <div className="flex items-center gap-4">
+          <img src="src/assets/todocheck.png" alt="icon" className="w-10 h-10" />
+          <h1 className="text-2xl font-bold">To-do</h1>
+        </div>
+
+        <div className="flex items-center ml-auto">
+          <h1 className="text-2xl font-bold">Why us</h1>
+        </div>
+      </header>
+      
     </div>
   );
 }
