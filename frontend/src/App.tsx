d6618a6e5c940ff4344342e368449dd77ae722fd
alohamat <matalohazk@gmail.com>
2025-07-19 16:49:14 -0300

    feat: new page and starting to make register form

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom'
 import MainPage from './pages/MainPage'
+import RegisterPage from './pages/RegisterPage'
 import './App.css'
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
   return (
       <Routes>
         <Route path='' element={<MainPage />}/>
+        <Route path='/register' element={<RegisterPage />} />
       </Routes>
   )
 }
